771ba035b12d7dc302021375144315bdce46004b
shina-hiroyuki20-fixer <shina.hiroyuki20@fixer.co.jp>
2020-07-14 17:37:28 +0900

    add: Added user pages :speech_balloon:
    追加しただけ。

---
+++ pages/users/_id.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p>
+      /users/_id.vue
+    </p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: '_id'
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/users/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p>
+      /users/index.vue
+    </p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'index'
+  }
+</script>
+
+<style scoped>
+
+</style>
